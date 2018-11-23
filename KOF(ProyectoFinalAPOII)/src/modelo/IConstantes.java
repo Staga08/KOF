b33e6df349d984a7ceb562f8793db8fa95d1eae2
brayangarces <brayangarces@localhost>
2018-11-23 14:02:53 -0500

    images

--- KOF(ProyectoFinalAPOII)/src/modelo/IConstantes.java
+++ KOF(ProyectoFinalAPOII)/src/modelo/IConstantes.java
@@ -4,7 +4,9 @@ public interface IConstantes {
 
 public final static String PLAYERONE="CHARACTER_ONE";
 public final static String PLAYERTWO="CHARACTER_TWO";
-//BAAHABABABA
+public final static int CANTIDADVIDA=20;
+public final static int DAÑOCAUSADO=5;
+
 
 
 }
