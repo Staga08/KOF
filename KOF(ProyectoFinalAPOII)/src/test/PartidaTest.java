fcb456e62d2b29fb1ed94673ac3df08a09a9f6e2
brayangarces <brayangarces@localhost>
2018-11-23 16:50:15 -0500

    bla bla

--- KOF(ProyectoFinalAPOII)/src/test/PartidaTest.java
+++ KOF(ProyectoFinalAPOII)/src/test/PartidaTest.java
@@ -11,18 +11,29 @@ import modelo.Partida;
 class PartidaTest {
 	
 	private Partida p;
+	private Jugador j1;
+	private Jugador j2;
+	private Jugador j3;
+	private Jugador j4;
+	
 	
 	void escenario1() {
 		p = new Partida();
 	}
 	
+	void escenario2() {
+		escenario1();
+		 j1 = new Jugador("Matakito", 100);
+		 j2 = new Jugador("Jinx", 200);
+		 j3 = new Jugador("Ekko", 300);
+		 j4 = new Jugador("Ahri", 400);
+		
+	}
+	
 	@Test
 	void agregarJugadoresTest() {
-		escenario1();
-		Jugador j1 = new Jugador("Matakito", 100);
-		Jugador j2 = new Jugador("Jinx", 200);
-		Jugador j3 = new Jugador("Ekko", 300);
-		Jugador j4 = new Jugador("Ahri", 400);	
+		escenario2();
+			
 		p.agregarJugadores(p.getJugadores(), j1);
 		p.agregarJugadores(p.getJugadores(), j2);
 		p.agregarJugadores(p.getJugadores(), j3);
