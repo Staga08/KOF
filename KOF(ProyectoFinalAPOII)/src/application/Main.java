7659ceeaaa30cff441085cfc5168784149e868eb
brayangarces <brayangarces@localhost>
2018-11-24 13:22:19 -0500

    x

--- KOF(ProyectoFinalAPOII)/src/application/Main.java
+++ KOF(ProyectoFinalAPOII)/src/application/Main.java
@@ -1,16 +1,27 @@
 package application;
 	
+import java.io.File;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import modelo.Partida;
 
 
 public class Main extends Application {
+	private static Stage primaryStage;
+	private static Partida partidilla;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			Pane root = (Pane)FXMLLoader.load(getClass().getResource("VentanaInicio.fxml"));
+			primaryStage.getIcons().add(new Image(new File("").toURI().toString()));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
@@ -20,7 +31,33 @@ public class Main extends Application {
 		}
 	}
 	
+	public static void mandarPantalla() {
+		try {
+			FXMLLoader loader=new FXMLLoader();
+			loader.setLocation(Main.class.getResource("Tablero.fxml"));
+			BorderPane root2 = loader.load();
+			
+			Stage stage2=new Stage();
+			stage2.setTitle("Buscaminas");
+			stage2.initModality(Modality.WINDOW_MODAL);
+			stage2.initOwner(primaryStage);
+			
+			Scene scene2=new Scene(root2);
+			stage2.setScene(scene2);
+			stage2.showAndWait();
+			primaryStage.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+	public static Partida getPartida() {
+		return partidilla;
+	}
+
+
 }
--- KOF(ProyectoFinalAPOII)/src/application/VentanaJuegoController.java
+++ KOF(ProyectoFinalAPOII)/src/application/VentanaJuegoController.java
@@ -5,12 +5,9 @@ import javafx.scene.image.ImageView;
 
 public class VentanaJuegoController {
 
-@FXML 
-private ImageView p1;
-@FXML
-private ImageView p2;
-@FXML
-private ImageView back;
+	@FXML private ImageView p1;
+	@FXML private ImageView p2;
+	@FXML private ImageView back;
 
 
 
