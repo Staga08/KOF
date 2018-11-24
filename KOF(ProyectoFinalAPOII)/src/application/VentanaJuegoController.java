84d905681174bb69751e08dc343161c1cea7ebcc
brayangarces <brayangarces@localhost>
2018-11-24 11:09:57 -0500

    windows update

---
+++ KOF(ProyectoFinalAPOII)/src/application/VentanaJuegoController.java
@@ -0,0 +1,22 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class VentanaJuegoController {
+
+@FXML 
+private ImageView p1;
+@FXML
+private ImageView p2;
+
+
+
+
+
+
+
+
+
+
+}
