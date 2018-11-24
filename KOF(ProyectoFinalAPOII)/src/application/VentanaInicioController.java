19ec47c5a6687fc0d669313e5e88423de8bcacc7
brayangarces <brayangarces@172.30.173.186>
2018-11-24 14:01:45 -0500

    ddd

--- KOF(ProyectoFinalAPOII)/src/application/VentanaInicioController.java
+++ KOF(ProyectoFinalAPOII)/src/application/VentanaInicioController.java
@@ -1,5 +1,46 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
 public class VentanaInicioController {
+
+	private Stage primaryStage;
+	@FXML Button prueba;
+
+	public void initialize() {
+	action();
+	}
+
+	public void action() {
+		
+		prueba.setOnAction(f->{
+			try {
+				FXMLLoader loader=new FXMLLoader();
+				loader.setLocation(Main.class.getResource("VentanaJuego.fxml"));
+				BorderPane root2 = loader.load();
+				
+				Stage stage2=new Stage();
+				stage2.setTitle("King of Fighters");
+				stage2.initModality(Modality.WINDOW_MODAL);
+				stage2.initOwner(primaryStage);
+				
+				Scene scene2=new Scene(root2);
+				stage2.setScene(scene2);
+				stage2.showAndWait();
+				primaryStage.close();
+			
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		});
+	}
+
 
 }
