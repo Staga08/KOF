e80041cf4644e0fa6cdbb6cee3195bcecf297a04
brayangarces <brayangarces@172.30.173.186>
2018-11-24 17:40:12 -0500

    jaja xd

--- KOF(ProyectoFinalAPOII)/src/application/VentanaPuntajesController.java
+++ KOF(ProyectoFinalAPOII)/src/application/VentanaPuntajesController.java
@@ -1,5 +1,12 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class VentanaPuntajesController {
+
+	@FXML private Button puntaje;
+	@FXML private Button nick;
+
 
 }
