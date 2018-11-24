b13bf91d5b5f92ba8dc3ebae9ab23a1bfbe874c8
brayangarces <brayangarces@172.30.173.186>
2018-11-24 17:45:56 -0500

    images has been fixed

--- KOF(ProyectoFinalAPOII)/src/modelo/IConstantes.java
+++ KOF(ProyectoFinalAPOII)/src/modelo/IConstantes.java
@@ -39,38 +39,38 @@ public interface IConstantes {
 	/** 
 	 * Imagenes del personaje RYO facing to the left
 	 * */
-	public final static String RYO_MOV_IZQ="/KOF(ProyectoFinalAPOII)/imagenes/ryo izq/bc3d376ed970bd95cf8788bd8010e5dd.gif";
-	public final static String RYO_ATAQ_IZQ="/KOF(ProyectoFinalAPOII)/imagenes/ryo izq/ryo the king of fighters 1 (26).gif";
+	public final static String RYO_MOV_IZQ="imagenes/ryo izq/bc3d376ed970bd95cf8788bd8010e5dd.gif";
+	public final static String RYO_ATAQ_IZQ="imagenes/ryo izq/ryo the king of fighters 1 (26).gif";
 	
 	/** 
 	 * Imagenes del personaje RYO facing to the right
 	 * */
-	public final static String RYO_MOV_DER="/KOF(ProyectoFinalAPOII)/imagenes/ryo der/oie_23204511wPl35c3p.gif";
-	public final static String RYO_ATAQ_DER="/KOF(ProyectoFinalAPOII)/imagenes/ryo der/oie_23204255GlIdSqBD.gif";
+	public final static String RYO_MOV_DER="imagenes/ryo der/oie_23204511wPl35c3p.gif";
+	public final static String RYO_ATAQ_DER="imagenes/ryo der/oie_23204255GlIdSqBD.gif";
 
 	/** 
 	 * Imagenes del personaje IORi facing to the left
 	 * */
-	public final static String IORI_MOV_IZQ="/KOF(ProyectoFinalAPOII)/imagenes/iori izq/kof-xiii-iori-yagami-sprite.gif";
-	public final static String IORI_ATAQ_IZQ="/KOF(ProyectoFinalAPOII)/imagenes/iori izq/24085ddb7cca1eec594abca5c93d3a5b.gif";
+	public final static String IORI_MOV_IZQ="imagenes/iori izq/kof-xiii-iori-yagami-sprite.gif";
+	public final static String IORI_ATAQ_IZQ="imagenes/iori izq/24085ddb7cca1eec594abca5c93d3a5b.gif";
 	
 	/** 
 	 * Imagenes del personaje IORI facing to the right
 	 * */
-	public final static String IORI_MOV_DER="/KOF(ProyectoFinalAPOII)/imagenes/iori der/image3.gif";
-	public final static String IORI_ATAQ_DER="/KOF(ProyectoFinalAPOII)/imagenes/iori der/iori_xiii___shogetsu_animations_by_blueblood2-db58wec.gif";
+	public final static String IORI_MOV_DER="imagenes/iori der/image3.gif";
+	public final static String IORI_ATAQ_DER="imagenes/iori der/iori_xiii___shogetsu_animations_by_blueblood2-db58wec.gif";
 
 	/** 
 	 * Imagenes del personaje TERRY facing to the left
 	 * */
-	public final static String TERRY_MOV_IZQ="/KOF(ProyectoFinalAPOII)/imagenes/terry izq/terry-12-stance.gif";
-	public final static String TERRY_ATAQ_IZQ="/KOF(ProyectoFinalAPOII)/imagenes/terry izq/Aucg2s.gif";
+	public final static String TERRY_MOV_IZQ="imagenes/terry izq/terry-12-stance.gif";
+	public final static String TERRY_ATAQ_IZQ="imagenes/terry izq/Aucg2s.gif";
 	
 	/** 
 	 * Imagenes del personaje TERRY facing to the right
 	 * */
-	public final static String TERRY_MOV_DER="/KOF(ProyectoFinalAPOII)/imagenes/terry der/oie_241746232c2JZbQ.gif";
-	public final static String TERRY_ATAQ_DER="/KOF(ProyectoFinalAPOII)/imagenes/terry der/oie_24174942DzuiwSRG.gif";
+	public final static String TERRY_MOV_DER="imagenes/terry der/oie_241746232c2JZbQ.gif";
+	public final static String TERRY_ATAQ_DER="imagenes/terry der/oie_24174942DzuiwSRG.gif";
 	
 	/** 
 	 * Imagenes de los iconos de seleccion de personajes
@@ -82,13 +82,13 @@ public interface IConstantes {
 	/** 
 	 * Imagenes de los escenarios del juego
 	 * */
-	public final static String CASA_EN_LLAMAS="/KOF(ProyectoFinalAPOII)/imagenes/escenarios/1.gif";
-	public final static String BARCO_EN_RUINAS="/KOF(ProyectoFinalAPOII)/imagenes/escenarios/2.gif";
-	public final static String AFUERAS_DEL_TEMPLO="/KOF(ProyectoFinalAPOII)/imagenes/escenarios/3.gif";
-	public final static String AFUERAS_TEMPLO_NIEVE="/KOF(ProyectoFinalAPOII)/imagenes/escenarios/4.gif";
-	public final static String BLACK_APOKALIPSIS="/KOF(ProyectoFinalAPOII)/imagenes/escenarios/5.gif";
-	public final static String TEMPLO_CUEVA="/KOF(ProyectoFinalAPOII)/imagenes/escenarios/6.gif";
-	public final static String AFUERAS_CHOZA="/KOF(ProyectoFinalAPOII)/imagenes/escenarios/7.gif";
+	public final static String CASA_EN_LLAMAS="imagenes/escenarios/1.gif";
+	public final static String BARCO_EN_RUINAS="imagenes/escenarios/2.gif";
+	public final static String AFUERAS_DEL_TEMPLO="imagenes/escenarios/3.gif";
+	public final static String AFUERAS_TEMPLO_NIEVE="imagenes/escenarios/4.gif";
+	public final static String BLACK_APOKALIPSIS="imagenes/escenarios/5.gif";
+	public final static String TEMPLO_CUEVA="imagenes/escenarios/6.gif";
+	public final static String AFUERAS_CHOZA="imagenes/escenarios/7.gif";
 
 
 
