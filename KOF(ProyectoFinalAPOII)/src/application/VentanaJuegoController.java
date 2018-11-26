db40eb943345192b0ca524a34df83a9b0af351f9
User <User@DESKTOP-D6OVAIF>
2018-11-26 00:02:32 -0500

    31 commit

--- KOF(ProyectoFinalAPOII)/src/application/VentanaJuegoController.java
+++ KOF(ProyectoFinalAPOII)/src/application/VentanaJuegoController.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.io.File;
+import java.security.SecureRandom;
 import java.sql.Time;
 import java.util.Currency;
 
@@ -17,10 +18,14 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 import modelo.IConstantes;
 
@@ -35,6 +40,7 @@ public class VentanaJuegoController {
 	@FXML private Timeline animation4;
 	@FXML private Timeline animation5;
 	@FXML private Timeline animation6;
+	@FXML private Label gameOver;
 	
 	/**
 	 * initialize(): void
@@ -122,6 +128,7 @@ public class VentanaJuegoController {
 		    wait.setOnFinished((e) -> {
 		       animacion6();
 		        wait.playFromStart();
+		        wait.stop();
 		    });
 		    wait.play();
 		
@@ -148,17 +155,37 @@ public class VentanaJuegoController {
 	
 	public void animacion7() {
 		animation6 = new Timeline(new KeyFrame(Duration.millis(30), f-> {
-			if (p1.getLayoutX()>200) {
-				animation6.stop();
-				
+			if (p1.getLayoutX()>535) {
+				Main.getPartida().get(Main.getPartida().getPosP()).atacar();
+				refreshP1();
+				animation6.stop();	
+				gameOver();
 			}
-			Main.getPartida().get(Main.getPartida().getPosP()).avanzarP1(1);
+			Main.getPartida().get(Main.getPartida().getPosP()).avanzarP1(6);
 			p1.setLayoutX(Main.getPartida().get(Main.getPartida().getPosP()).getPosX());
-			Main.getPartida().get(Main.getPartida().getPosP2()).avanzarP2(3);
+			Main.getPartida().get(Main.getPartida().getPosP2()).avanzarP2(1);
 			p2.setLayoutX(Main.getPartida().get(Main.getPartida().getPosP2()).getPosX());
 		}));
 		animation6.setCycleCount(Timeline.INDEFINITE);
 		animation6.play();
+	}
+	
+	public void gameOver() {
+		
+		gameOver.setText("GAME OVER");
+		SecureRandom random = new SecureRandom();
+		int puntaje = random.nextInt(1000)+100;
+		Main.getPartida().inOrden(Main.getPartida().getJugadores());
+		Main.getPartida().getMejoresPuntajes().get(Main.getPartida().
+				getMejoresPuntajes().size()-1).setPuntaje(puntaje);
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("GAME OVER");
+		alert.setHeaderText(null);
+		alert.setContentText("Tu puntaje es: "+Integer.toString(Main.getPartida().
+				getMejoresPuntajes().get(Main.getPartida().getMejoresPuntajes().size()-1).getPuntaje()));
+		Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+		stage.getIcons().add(new Image(new File("imagenes/Icon.png").toURI().toString()));
+		alert.show();
 	}
 	
 	public void refreshP1() {
